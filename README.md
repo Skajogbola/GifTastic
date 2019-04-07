84c5c3418988b1f1170a621e131f8f5ce6c40b22
Simmie <skajogbola@users.noreply.github.com>
2019-04-07 00:49:07 -0400

    commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # GifTastic
+Description: 
 
-In this assignment, I use the GIPHY API to make a dynamic web page that populates sports gifs . To finish this task, I call the GIPHY API and use JavaScript and jQuery to change the HTML of my site.
+Use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To finish this task, you must call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
+
+Here's the solution:
+
+I created an sport App full of gifs using the giphy api.
+I made an array of the types of sports and made buttons out of them.
+When you click on a button, 10 images are retrieved from giphy and displayed on the page.
+When you click on the image, the gif plays. When you click again, the gif stops playing.
+The player can add a new sport from the form on the right side of the page. The actions will be added as a button to the array of the types of sports.
+The rating of each gif will also appear.
+
+Technical approach:
+To finish this task, I created the html and CSS (using Bootstrap) files and called the GIPHY API. I also used JavaScript and jQuery to change the HTML of my site.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,40 +18,35 @@ $(document).ready(function () {
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
-                // Creates a div to hold the movie
                 var sportsDiv = $("<div>");
                 $('#buttons-view').append(sportsDiv);
 
                 // Retrieves the Rating Data
                 console.log(response.data[i].rating);
                 var rating = results[i].rating;
-                // Creates an element to have the rating displayed
                 var ratedText = $("<p>").text("Rating: " + rating);
-                // Displays the rating
                 sportsDiv.prepend(ratedText);
 
                 //Retrieves the image Data
                 var sportImage = $("<img class='gif'>");
                 sportImage.attr("src", results[i].images.fixed_height_still.url);
-                //adds element data-state = still
                 sportImage.attr("data-state", "still");
-                //adds element data-still with paused gif
-                sportImage.attr("data-still", results[i].images.fixed_height_still.url)
-                //adds element data-animate with moving gif
+                sportImage.attr("data-still", results[i].images.fixed_height_still.url);
                 sportImage.attr("data-animate", results[i].images.fixed_height.url)
 
                 sportsDiv.append(sportImage);
+                $(".insert-gif").append(sportsDiv);
+                
             }
         }).fail(function (err) {
             throw err;
         });
     });
+
     //function stores input to data-search and create a button
-    // $("#buttons-view").on("click",function(){
     function createButton() {
         //delete prior gifs to add over gif buttons
         $("#buttons-view").empty();
-        //creates new button, creates gif-button class, and add into array
         for (var i = 0; i < topics.length; i++) {
             var a = $("<button>");
             a.addClass("gif-button");
@@ -68,23 +63,14 @@ $(document).ready(function () {
         topics.push(newSports);
         createButton();
     });
-
-    // Calling the renderButtons function to display the initial list of movies
     createButton();
 
     $(document).on("click", ".gif", function () {
-
-        //when gif is paused, it can animate when clicked
         var state = $(this).attr("data-state");
 
         if (state === 'still') {
-            //create animate equals to value in element data-animate
             var animate = $(this).attr("data-animate");
-
-            //overwrite element src with variable animate
             $(this).attr("src", animate);
-
-            //overwrite element data-state to animate
             $(this).attr("data-state", "animate");
 
         } else if (state != 'still') {
